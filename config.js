ae207af001c5fc90f6353f792b6ba22c509e441a
CodeCrypto <58365830+Pilhamre@users.noreply.github.com>
2021-01-04 18:11:26 +0100

    update

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 var config = {
     CODE : '1111112',
-    ELEMENT : '<a href="https://ludwig.gg/"><img src="./Images/lud.png" id="title" style="width: 50vh; display: none; padding: 12vh;"></a>'
+    ELEMENT : '<a href="https://ludwig.gg/"><img src="./Images/lud.png" id="title" style="width: 50vh; display: none; padding: 7vh;"></a>',
+    AUDIO : '<audio id="audio" controls autoplay loop><source src="./nothing.mp3" type="audio/mpeg">Your browser does not support the audio element.</audio>'
 }
